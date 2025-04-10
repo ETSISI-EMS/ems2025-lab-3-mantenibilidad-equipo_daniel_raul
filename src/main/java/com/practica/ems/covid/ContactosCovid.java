41c9ad8e24336dee8131a9507dd999aa759995f6
daniel.lai <daniel.lai@alumnos.upm.es>
2025-04-10 12:32:24 +0200

    refactor switch crearPosicionPersona

--- src/main/java/com/practica/ems/covid/ContactosCovid.java
+++ src/main/java/com/practica/ems/covid/ContactosCovid.java
@@ -220,30 +220,11 @@ public class ContactosCovid {
 
 	private PosicionPersona crearPosicionPersona(String[] data) {
 		PosicionPersona posicionPersona = new PosicionPersona();
-		String fecha = null, hora;
-		float latitud = 0, longitud;
-		for (int i = 1; i < Constantes.MAX_DATOS_LOCALIZACION; i++) {
-			String s = data[i];
-			switch (i) {
-			case 1:
-				posicionPersona.setDocumento(s);
-				break;
-			case 2:
-				fecha = data[i];
-				break;
-			case 3:
-				hora = data[i];
-				posicionPersona.setFechaPosicion(parsearFecha(fecha, hora));
-				break;
-			case 4:
-				latitud = Float.parseFloat(s);
-				break;
-			case 5:
-				longitud = Float.parseFloat(s);
-				posicionPersona.setCoordenada(new Coordenada(latitud, longitud));
-				break;
-			}
-		}
+		FechaHora fecha = parsearFecha(data[2], data[3]);
+		Coordenada coord = new Coordenada(Float.parseFloat(data[4]), Float.parseFloat(data[5]));
+		posicionPersona.setDocumento(data[1]);
+		posicionPersona.setFechaPosicion(fecha);
+		posicionPersona.setCoordenada(coord);
 		return posicionPersona;
 	}
 	
